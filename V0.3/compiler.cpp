cc75c7330929d6fd2b0b7a8cae35cd1293ef4c45
Draglesss <dangerpop-otmane@hotmail.com>
2022-03-15 09:32:03 +0100

    V0.3.2

---
+++ V0.3/compiler.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <bits/stdc++.h>
+#include <windows.h>
+#include "library.h"
+#pragma execution_character_set( "utf-8" )
+#define colWidth 15
+using namespace std;
+//* OOP aproach for easy debugging
+class element {
+	vector <string> value;
+	string name;
+	public :
+	element(string name) : name(name) {
+		cout << name <<" element created" << endl;
+	}
+	void push(const string& str) {
+		value.push_back(str);
+		cout << str << " pushed to " << name << endl;
+	}
+	string operator [](int i) const {
+		return value[i];
+	}
+	int size() const {
+		return value.size();
+	}
+	~element() {
+		value.clear();
+	}
+};
+int main()
+{
+	string line, word, Dict_word, File_word;
+	vector <string> Dict_words, File_words;
+	element keys("keys"), others("others"), numbers("numbers"), operators("operators"), strings("strings"), IDs("IDs");
+	ifstream File("File.txt"); //* step 1 : reading file
+	ifstream Dictionary("Dictionnaire.txt"); //* step 2 : linking Dictionary
+	bool pushed = false;
+	while(getline(Dictionary, Dict_word, '\n')) {
+		Dict_words.push_back(Dict_word);
+	}
+	while(getline(File, File_word, ' ')) {
+		File_words.push_back(File_word);
+	}
+	File.close();
+	//* For loop to treat every word in file.txt
+	for (int i = 0; i < File_words.size(); i++) {
+		//* Keys Detection
+		for (int z = 0; z < Dict_words.size(); z++) {
+			if (File_words[i].compare(Dict_words[z]) == 0) {
+				keys.push(File_words[i]);
+				pushed = true;
+				break;
+			}
+		}
+		//* Numbers Detection
+		if (isnumber(File_words[i]) and not_a_space(File_words[i])) {
+				numbers.push(File_words[i]);
+				pushed = true;
+		}
+		if (!pushed and (not_a_space(File_words[i]) == true) and !exists(File_words[i], '\n')) {
+			if(isID(File_words[i])) {
+				IDs.push(File_words[i]);
+				pushed = true;
+			}
+			else if(isstring(File_words[i])) {
+				strings.push(File_words[i]);
+				pushed = true;
+			}
+			else if(isoperator(File_words[i])) {
+				operators.push(File_words[i]);
+				pushed = true;
+			}
+			else if(!pushed and !char_comp(File_words[i], ';')){
+				others.push(File_words[i]);
+				pushed = true;
+			}
+		}
+		pushed = false;
+	}
+	//* Console Output
+	cout << setfill('*') << setw(3*colWidth) << "*" << endl;
+	cout << setfill(' ') << fixed;
+	cout << setw(colWidth) << "mots" << setw(colWidth) << "valeur" << endl;
+	cout << setfill('*') << setw(3*colWidth) << "*" << endl;
+	cout << setfill(' ') << fixed;
+	//* List 1 : IDs
+	for(int i = 0; i < IDs.size(); i++) {
+		cout << setprecision(0) << setw(colWidth) << IDs[i] << setprecision(4) << 
+		setw(colWidth)<< "ID" << endl;
+	}
+	//* List 2 : key words
+	for(int i = 0; i < keys.size(); i++) {
+		cout << setprecision(0) << setw(colWidth) << keys[i] << setprecision(4) << 
+		setw(colWidth)<< "mot cle" << endl;
+	}
+	//* List 3 : numbers
+	for(int i = 0; i < numbers.size(); i++) {
+		cout << setprecision(0) << setw(colWidth) << numbers[i] << setprecision(4) << 
+		setw(colWidth)<< "nombre" << endl;
+	}
+	//* List 4 : operators
+	for(int i = 0; i < operators.size(); i++) {
+		cout << setprecision(0) << setw(colWidth) << operators[i] << setprecision(4) << 
+		setw(colWidth)<< "operateur" << endl;
+	}
+	//* List 5 : strings
+	for(int i = 0; i < strings.size(); i++) {
+		cout << setprecision(0) << setw(colWidth) << strings[i] << setprecision(4) << 
+		setw(colWidth)<< "chaine" << endl;
+	}
+	//* List 6 : unidentified
+	for(int i = 0; i < others.size(); i++) {
+		cout << setprecision(0) << setw(colWidth) << others[i] << "      " <<
+		setw(colWidth)<< "erreurs potentielles" << endl;
+	}
+  	return 0;
+}
---
+++ V0.3/library.h
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <bits/stdc++.h>
+using namespace std;
+
+//* returns length of a string
+int len(const string& str)
+{
+    int length = 0;
+    for (int i = 0; str[i] != '\0'; i++) {  
+        length++;
+    }
+    return length;
+}
+//* returns true if strings in parametres are the same
+bool compare(const string &s1, const string &s2) {
+	int count = 0;
+	for (int i = 0; i < s1.size(); i++) {
+		if (s1[i] == s2[i])
+			count++;
+	}
+	if (count == s1.size())
+		return true;
+	else
+		return false;
+}
+//* returns if the charactere in param exists in string
+bool exists(const string& string, char c) {
+	bool exists;
+	for (int i = 0; i < string.size(); i++) {
+		if (string[i] == c)
+			exists = true;
+	}
+	if (exists) 
+		return true;
+	else
+		return false;
+}
+
+//* NOT USED : splits a string into words
+string split(const string &chaine) {
+	char str[100], *ptr;
+	string result;
+	for(int i = 0; i <= len(chaine); i++) {
+		str[i] = chaine[i];
+	}
+	ptr = strtok(str, " ");
+	cout << str << endl;
+	for(int i = 0; i <= len(ptr); i++) {
+		result[i] = ptr[i];
+		cout << ptr[1];
+		break;
+	}
+	return result;
+}
+//* checks if a string is an operator
+bool isoperator(const string& s) {
+	vector <string> operators = {"*", "=", "==", "+", "-", "/", "%", "!", "#", "<", ">", "!=", ")", "(", "{", "}"};
+    for( int i = 0; i < operators.size(); i++) {
+        if (s == operators[i])
+            return true;
+    }
+    return false;
+}
+bool isstring(const string& str) {
+	int count = 0;
+	for (char const &c : str) {
+		if(c == '"')
+			count++;
+	}
+	if(count == 2)
+		return true;
+    else 
+        return false;
+}
+bool char_comp(const string& str, char c) {
+	for(int i = 0 ; i < str.size() ; i++) {
+		if(str[i] == c)
+			return true;
+	}
+	return false;
+}
+bool isnumber(const string& str) {
+    int count = 0;
+	for (char const &c : str) {
+        if (!isdigit(c) and c != '.') {
+			return false;
+        }
+        if (c == '.')
+                count++;
+    }
+    if(count > 1)
+        return false;
+    return true;
+}
+bool not_a_space(const string& str) {
+    char* token = strtok(const_cast<char*>(str.c_str()), " ");
+    while (token != NULL) {
+        if (*token != ' ')  
+            return true;
+    }
+    return false;
+}
+
+bool isID(const string& str) {
+    for(auto c : str) {
+        if (static_cast<unsigned char>(c) < 64 or static_cast<unsigned char>(c) > 90 ) {
+            if (static_cast<unsigned char>(c) < 97 or static_cast<unsigned char>(c) > 122) {
+                if (static_cast<unsigned char>(c) != 95)
+                    return false;
+            }
+        }
+    }
+    if(isdigit(str[0]) == true)
+        return false;
+    return true;
+}
